631cdc7ef663685967907f867e22832f5cb0a376
zoe <zoe@DESKTOP-I5R1J0O>
2018-04-26 17:09:27 +0800

    verifyThread做了一些更改

--- src/joy/aksd/listenAndVerifyThread/verifyThread.java
+++ src/joy/aksd/listenAndVerifyThread/verifyThread.java
@@ -19,6 +19,7 @@ public class verifyThread extends Thread{
             } catch (InterruptedException e) {
                 return;
             }
+            //时间和顺序双重把控，确保即使发生网络延迟，记录的顺序依旧正确
             if (verifyStamp(record) && verifyTime(record)) {
                 String key = byteToString(record.getLockScript());
                 freshRecord.put(key, record);
@@ -27,17 +28,23 @@ public class verifyThread extends Thread{
         }
     }
 
-    private boolean verifyTime(Record record) {//此处验证需要扩展，比如时间上后面的先于前面的到达，需要进行重新入队处理，目前仅做简单处理
+    private boolean verifyTime(Record record) {//此处验证需要扩展，比如需要时间上后面的先于前面的到达，进行重新入队处理，目前仅做简单处理
         String key=byteToString(record.getLockScript());
         boolean resflag = false;
+        //比较该用户当前记录与上一记录时间，当前时间应小于上一记录时间
         if (byteToInt(freshRecord.get(key).getTime())<byteToInt(record.getTime())) {
-        	if( byteToInt(record.getTime())>(errorTime -NTPTime) && byteToInt(record.getTime()) <= (errorTime + NTPTime)) 
+        	//判断当前记录时间是否在允许误差范围内
+        	if( byteToInt(record.getTime())>(errorTime -NTPTime) && byteToInt(record.getTime()) <= (errorTime + NTPTime)) { 
         		resflag = true;
-        	System.out.println("该记录时间上先于前面记录到达，将其重新入队");
+        		System.out.println("该记录时间验证通过");
+        	}
+        	else
+        	{
+        		System.out.println("该记录时间超时，已丢弃");
+        	}
         }
-            
         else {
-            effectiveRecord.add(record);
+        	effectiveRecord.add(record);
             System.out.println("已将该记录重新入队");
         }
 		return resflag;
@@ -47,14 +54,12 @@ public class verifyThread extends Thread{
         boolean resflag = false;
         if (byteToInt(freshRecord.get(key).getOrderStamp())+1==byteToInt(record.getOrderStamp())) {
         	resflag = true;
-        	System.out.println("该记录顺序上先于前面记录到达，将其重新入队");
+        	System.out.println("该记录顺序验证通过");
         }           
         else {
         	effectiveRecord.add(record);
         	System.out.println("已将该记录重新入队");
         }
-		return resflag;
-        	
+		return resflag;	
     }
-
 }
